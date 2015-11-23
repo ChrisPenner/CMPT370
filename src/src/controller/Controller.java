70d64d8dab287a8bb57f108a35d3ace91fa76705
evansalter <evanearlsalter@gmail.com>
2015-11-23 12:09:00 -0600

    Added game time, play/stop works, rate change works

--- src/src/controller/Controller.java
+++ src/src/controller/Controller.java
@@ -5,17 +5,26 @@ import java.util.LinkedList;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import views.*;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Controller {
 	
 	LinkedList<Robot>[] teams;
 	int edgeLength;
+	static Timer gameTimer;
+	
+	static boolean gameIsRunning = false;
+	
+	static int timerLoopCount = 0;
+	static int gameRate = 50;
+	static int currentTurn = 1;
 	
 	@SuppressWarnings("unchecked")
 	public Controller() {
 		// initialize the controller
 		teams = (LinkedList<Robot>[]) new LinkedList<?>[6];
-		
+		gameTimer = new Timer();
 	}
 	
 	public void start() {
@@ -36,10 +45,15 @@ public class Controller {
 	
 	public static void play() {
 		System.out.println("Controller: Play was pressed");
+		
+		gameIsRunning = true;
+		gameTimer.schedule(new GameLoop(), 0, 1000/60);
+		
 	}
 	
 	public static void stop() {
 		System.out.println("Controller: Stop was pressed");
+		gameIsRunning = false;
 	}
 	
 	public static void step() {
@@ -48,6 +62,25 @@ public class Controller {
 	
 	public static void changeRate(int rate) {
 		System.out.println("Controller: Rate was changed to " + rate);
+		gameRate = rate;
+	}
+	
+	private static class GameLoop extends TimerTask {
+		
+		public void run() {
+			
+			if(timerLoopCount % (101-gameRate) == 0) {
+				System.out.println("Current turn: " + currentTurn);
+				currentTurn++;
+				timerLoopCount = 0;
+			}
+			timerLoopCount++;
+			
+			if(!gameIsRunning){
+				this.cancel();
+			}
+		}
+		
 	}
 
 }
